1e1fbf50b1410d3545adc4d5a0091a151e9bab72
Amanda Cornell <acornell94@gmail.com>
2019-02-12 19:40:10 -0600

    practice with tuples

---
+++ tuple_colors.py
@@ -0,0 +1,10 @@
+colors = ('red', 'orange', 'yellow', 'green', 'blue')
+for color in colors:
+    print(color)
+
+print("\nOriginal colors:")
+print(colors)
+
+colors = ('indigo', 'violet', 'white', 'black', 'grey')
+print("\nModified colors:")
+print(colors)
