265b3b477ef4d7419f48f3e0f7bfa42bc64a70ba
Amanda Cornell <acornell94@gmail.com>
2019-02-12 19:31:48 -0600

    practice in conditional statements

---
+++ car_conditions.py
@@ -0,0 +1,12 @@
+car = 'honda'
+print("Is car == 'honda'? I predict true.")
+print(car == 'honda')
+
+print("\nIs car == 'Honda'? I predict false.")
+print(car=="Honda")
+
+print("\nIs car.title() == 'Honda.lower()'? I predict true.")
+print(car.title() == 'Honda')
+
+print("\nIs car != 'subaru'? I predict true.")
+print(car != 'subaru')
