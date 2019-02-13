6d1ea3e5dd0cfa0457880a21c18d78b31a0acfc7
Amanda Cornell <acornell94@gmail.com>
2019-02-12 19:37:47 -0600

    slicing practice

---
+++ slicing_practice.py
@@ -0,0 +1,9 @@
+numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight']
+
+print("The first three items in the list are as follows:")
+print(numbers[0:3])
+print("\nThe four items in the middle are as follows:")
+print(numbers[2:7])
+print("\nThe last three items are as follows:")
+print(numbers[5:])
+
