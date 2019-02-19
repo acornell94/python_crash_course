3863b0ca6a85a7bb7e3d07dc1f13439b26084128
Amanda Cornell <acornell94@gmail.com>
2019-02-18 20:13:49 -0600

    practice with if statements

---
+++ stages_of_life_tiy.py
@@ -0,0 +1,14 @@
+age = 68
+
+if age < 2:
+    print("That person is a baby.")
+elif age >= 2 and age < 4:
+    print("That person is a toddler.")
+elif age >= 4 and age < 13:
+    print("That person is a kid.")
+elif age >= 13 and age < 20:
+    print("That person is a teenager.")
+elif age >= 20 and age < 65:
+    print("That person is an adult.")
+elif age >= 65:
+    print("That person is an elder.")
