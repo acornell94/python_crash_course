94c214aa22cff06d9443adcda50378f073b52f8a
Amanda Cornell <acornell94@gmail.com>
2019-02-18 20:08:10 -0600

    practice with if statements

---
+++ pizza2.py
@@ -0,0 +1,8 @@
+toppings = []
+
+if toppings:
+    for topping in toppings:
+        print("Adding " + topping)
+    print("Your pizza is finished!")
+else:
+        print("Are you sure you would like a plain pizza?")
