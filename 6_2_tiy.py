a23f2d87b4f6f7da9bd07652aec5c6ede7614ead
Amanda Cornell <acornell94@gmail.com>
2019-02-19 19:34:04 -0600

    practice with dictionaries

---
+++ 6_2_tiy.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'sarah': 6,
+    'kate': 1,
+    'ann': 12,
+    'susan': 40,
+    'lori': 2,
+    }
+
+print("Sarah's favorite number is " + str(favorite_numbers['sarah']) + ".")
+print("Kate's favorite number is " + str(favorite_numbers['kate']) + ", and Lori's favorite number is " + 
+    str(favorite_numbers['lori']) + ".")
+print("Ann's favorite number is " + str(favorite_numbers['ann']) + ".")
+print("Lastly, Susan's favorite number is " + str(favorite_numbers['susan']) + ".")
