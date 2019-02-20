6c619b71b2fe0780ad853ec93de3a8f5384e25d9
Amanda Cornell <acornell94@gmail.com>
2019-02-19 19:35:08 -0600

    practice with dictionaries

---
+++ 6_3_tiy.py
@@ -0,0 +1,10 @@
+user_0 = {
+    'first': 'john',
+    'last': 'smith',
+    'username': 'jsmith',
+    }
+
+for key, value in user_0.items():
+    print("Key: " + key)
+    print("Value: " + value + "\n")
+    
