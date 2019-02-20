f7e5f16d94b8310d66df715f25d5cc2af16c90e3
Amanda Cornell <acornell94@gmail.com>
2019-02-19 19:33:06 -0600

    practice with dictionaries

---
+++ 6_1_tiy.py
@@ -0,0 +1,4 @@
+person = {'first_name': 'john', 'last_name': 'doe', 'age': 30, 'city': 'chicago'}
+
+print("His name is " + person['first_name'].title() + " " + person['last_name'].title() + ".")
+print("He is a " + str(person['age']) + "-year-old man from " + person['city'].title() + ".")
