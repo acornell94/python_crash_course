ed16a24bae2da352d770cf1b7d13660fb1359e54
Amanda Cornell <acornell94@gmail.com>
2019-02-23 22:14:26 -0600

    practice with functions

---
+++ t_shirt_tiy.py
@@ -0,0 +1,5 @@
+def make_shirt(size, message):
+    print('Yours will be a ' + size + ' shirt that says "' + message + '".')
+
+make_shirt('small', 'Hello!')
+make_shirt(message='Hello!', size='small')
