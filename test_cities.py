97cf8f237c87f80e6893feb10538b86597331800
Amanda Cornell <acornell94@gmail.com>
2019-05-13 08:15:52 -0500

    practice with unit tests

--- test_cities.py
+++ test_cities.py
@@ -5,9 +5,14 @@ from city_functions import location
 class CityTestCase(unittest.TestCase):
     """Test functionality of city_functions.py"""
     
-    def test_location(self):
+    def test_city_country(self):
         """Test whether a city, country format works."""
         tested_location = location("montreal", "canada")
         self.assertEqual(tested_location, 'Montreal, Canada')
+
+    def test_city_state_country(self):
+        """Test city, state, country format."""
+        tested_location = location("Chicago", "United States of America", "Illinois")
+        self.assertEqual(tested_location, "Chicago, Illinois, United States Of America")
 
 unittest.main()
